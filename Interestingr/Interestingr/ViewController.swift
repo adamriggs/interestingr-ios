26fc7dc2172ade1ffb4ba745aa5d1bc3d4bae2ef
Adam Riggs <adam@adamriggs.com>
2014-09-30 01:09:26 -0400

    done for the night

--- Interestingr/Interestingr/ViewController.swift
+++ Interestingr/Interestingr/ViewController.swift
@@ -22,7 +22,19 @@ class ViewController: UIViewController {
         var imageData :NSData = NSData.dataWithContentsOfURL(url,options: NSDataReadingOptions.DataReadingMappedIfSafe, error: &err)
         
         mainImage.contentMode = .ScaleAspectFit
-        mainImage.image = UIImage(data:imageData)
+        
+        var image = UIImage(data:imageData)
+        
+        let size = CGSizeApplyAffineTransform(image.size, CGAffineTransformMakeScale(0.5, 0.5))
+        let hasAlpha = false
+        let scale: CGFloat = 0.0 // Automatically use scale factor of main screen
+        
+        UIGraphicsBeginImageContextWithOptions(size, !hasAlpha, scale)
+        image.drawInRect(CGRect(origin: CGPointZero, size: size))
+        
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        mainImage.image = scaledImage
         
     }
 

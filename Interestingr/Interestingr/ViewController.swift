b51ae7e6cf8795e116839fe551c71f5e89f815b3
Adam Riggs <adam@adamriggs.com>
2014-09-29 23:29:36 -0400

    image displays in app

--- Interestingr/Interestingr/ViewController.swift
+++ Interestingr/Interestingr/ViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet var mainImage: UIImageView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        var urlString:String = "https://farm6.staticflickr.com/5557/15057740736_00fc1f196a.jpg"
+//        var url:NSURL = NSURL.URLWithString(urlString)
+//        //println("url=="+interestingnessArray[id])
+//        mainImage.image = NSImage(byReferencingURL: url)
+        
+        var url = NSURL.URLWithString("https://farm6.staticflickr.com/5557/15057740736_00fc1f196a.jpg");
+        var err: NSError?
+        var imageData :NSData = NSData.dataWithContentsOfURL(url,options: NSDataReadingOptions.DataReadingMappedIfSafe, error: &err)
+        mainImage.image = UIImage(data:imageData)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func scaleImage(){
+        
     }
 
 
